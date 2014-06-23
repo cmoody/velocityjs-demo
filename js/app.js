0e4ba69f451aa73111e8168c3d5d38c8f226bf76
Chase Moody <chase.moody@mutualmobile.com>
2014-06-23 13:20:03 -0500

    Updates

--- js/app.js
+++ js/app.js
@@ -1,50 +1,7 @@
-// Custom Easing Function
-$.easing["myCustomEase"] = function(p) {
-    return 1 - (Math.cos(p * -20 * Math.PI) * Math.exp(-p * 2));
-};
+var $box = $('.box');
 
-// $.easing["spring"] = function(p) {
-//     return 1 - (Math.cos(p * 4.5 * Math.PI) * Math.exp(-p * 6));
-// };
+$box.velocity({ translateY: 125 }, 1150, [ 300, 8 ]);
 
-// latest update might be
-// velocity.Easings["spring"] = function(p) {}
-
-$('.box').velocity(
-	{
-		translateY: [0, 50],
-		translateX: [0, 50],
-	}, 
-	{
-		duration: 1000, 
-		delay: 2000, 
-		easing: "easeInOutElastic",
-		loop: 4
-	}
-);
-
-$('.box2').velocity(
-	{
-		translateY: [0, 50],
-		translateX: [0, 50],
-	}, 
-	{
-		duration: 1000, 
-		delay: 2000, 
-		easing: "spring",
-		loop: 4
-	}
-);
-
-$('.box3').velocity(
-	{
-		translateY: [0, 50],
-		translateX: [0, 50],
-	}, 
-	{
-		duration: 1000, 
-		delay: 2000, 
-		easing: "myCustomEase",
-		loop: 4
-	}
-);
+// $box.velocity("transition.slideUpIn", { stagger: 400 })
+//     .delay(750)
+//     .velocity("transition.slideUpOut", { stagger: 400 });
